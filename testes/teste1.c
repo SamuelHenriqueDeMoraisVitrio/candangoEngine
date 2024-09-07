48ee960ba08a136ce7f0b7416287fc5b4505f1d6
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-09-07 00:11:49 -0300

    tt

--- testes/teste1.c
+++ testes/teste1.c
@@ -6,7 +6,7 @@ int main(){
   const char *path_html = "testes/teste_home.html";
   char *file = dtw_load_string_file_content(path_html);
 
-  printf("\n\t%s\n\n", file);
+  printf("\n%s\n\n", file);
 
   free(file);
   
