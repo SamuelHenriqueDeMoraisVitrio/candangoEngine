2ab78059a92d8f3e39f3b029007aa65d31edb679
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2025-03-09 19:22:13 -0300

    tt

--- src/functions/formater_string/variable/def.variable.c
+++ src/functions/formater_string/variable/def.variable.c
@@ -7,7 +7,19 @@
 
 
 
+char *Candango_variable_text_formatter(Candango_args_render *self, const char *text_working, size_t block_finished_cod_lua, LuaCEmbed *machine){
+  Candango_adicionar_ao_buffer_lua(self, text_working, block_finished_cod_lua);
+  const char *result_evaluet = Candango_lua.get_string_evaluation(machine, self->strings->text_to_work);
+  if(Candango_lua.has_errors(machine)){
+    return Candango_lua.get_error_message(machine);
+  }
+  size_t size_result = strlen(result_evaluet);
+  Candango_Realloc_Buffer(self, size_result);
+  Candango_adicionar_ao_buffer(self, result_evaluet, size_result);
+  Candango_reset_buffer_lua(self);
 
+  return NULL;
+}
 
 
 
